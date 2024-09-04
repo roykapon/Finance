2cfdac516bfb754228d3c07b80b54c99ce807f1d
roykapon <roykapon@gmail.com>
2024-09-04 18:18:52 +0300

    Changed data loading to account for existing data and split into test and train directories

--- get_data.py
+++ get_data.py
@@ -1,9 +1,13 @@
+import random
 import numpy as np
 import pandas as pd
 import yfinance as yf
 from alpha_vantage.fundamentaldata import FundamentalData
 import os
+from os.path import join as pjoin
+import shutil
 
+DATA_TMP_DIR = "data_tmp"
 DATA_TYPES = ["stock_prices", "income_statement", "balance_sheet", "cash_flow", "corporate_actions"]
 MIN_STD = 1e-5
 
@@ -12,8 +16,8 @@ alpha_vantage_api_key = "7OLTH3MD4449GIQ3"
 
 # List of the 100 most traded stock symbols (Example list)
 # fmt: off
-# most_traded_stocks = ["AAPL", "MSFT", "AMZN", "TSLA", "GOOGL", "GOOG", "META", "NVDA", "PYPL", "ADBE", "NFLX", "INTC", "CSCO", "PFE", "MRK", "DIS", "V", "MA", "JPM", "BAC", "WMT", "T", "PG", "KO", "PEP", "XOM", "CVX", "BA", "NKE", "MCD", "IBM", "ORCL", "ACN", "AVGO", "TXN", "COST", "QCOM", "AMD", "SPGI", "AMT", "MDT", "HON", "LLY", "UNP", "UNH", "UPS", "NEE", "AXP", "LIN", "LOW", "MS", "GS", "BLK", "BK", "TMO", "ABBV", "CVS", "RTX", "MMM", "INTU", "SYK", "CAT", "CI", "FIS", "DHR", "HUM", "EL", "LMT", "ADP", "SCHW", "ZTS", "ISRG", "GE", "USB", "FDX", "ICE", "SBUX", "CME", "NOW", "TMUS", "GM", "DE", "MMC", "ANTM", "GILD", "ADSK", "PLD", "ECL", "APD", "BSX", "MO", "FISV", "HCA", "DG", "F", "CL", "D", "DUK", "SO", "ETN",]
-most_traded_stocks = []
+most_traded_stocks = ["AAPL", "MSFT", "AMZN", "TSLA", "GOOGL", "GOOG", "META", "NVDA", "PYPL", "ADBE", "NFLX", "INTC", "CSCO", "PFE", "MRK", "DIS", "V", "MA", "JPM", "BAC", "WMT", "T", "PG", "KO", "PEP", "XOM", "CVX", "BA", "NKE", "MCD", "IBM", "ORCL", "ACN", "AVGO", "TXN", "COST", "QCOM", "AMD", "SPGI", "AMT", "MDT", "HON", "LLY", "UNP", "UNH", "UPS", "NEE", "AXP", "LIN", "LOW", "MS", "GS", "BLK", "BK", "TMO", "ABBV", "CVS", "RTX", "MMM", "INTU", "SYK", "CAT", "CI", "FIS", "DHR", "HUM", "EL", "LMT", "ADP", "SCHW", "ZTS", "ISRG", "GE", "USB", "FDX", "ICE", "SBUX", "CME", "NOW", "TMUS", "GM", "DE", "MMC", "ANTM", "GILD", "ADSK", "PLD", "ECL", "APD", "BSX", "MO", "FISV", "HCA", "DG", "F", "CL", "D", "DUK", "SO", "ETN",]
+# most_traded_stocks = []
 # fmt: on
 
 basedate = pd.Timestamp("0001-1-1")
@@ -76,18 +80,17 @@ def get_corporate_actions(symbol: str):
 
 
 # Ensure the data directory exists
-if not os.path.exists("data"):
-    os.makedirs("data")
+if not os.path.exists(DATA_TMP_DIR):
+    os.makedirs(DATA_TMP_DIR)
 
 # Loop over the most traded stocks
 for symbol in most_traded_stocks:
-    company_dir = f"data/{symbol}"
+    company_dir = pjoin(DATA_TMP_DIR, symbol)
     print(f"Processing {symbol}...")
 
     if not os.path.exists(company_dir):
         try:
 
-            os.makedirs(company_dir)
             # Fetch data
             stock_prices = get_stock_prices(symbol)
             income_statement, balance_sheet, cash_flow = get_financial_statements(symbol)
@@ -100,19 +103,22 @@ for symbol in most_traded_stocks:
                 data_dict[data_type] = process_data(data)
 
             # Save each table to a separate CSV file
+            os.makedirs(company_dir)
+            
             for data_type, data in data_dict.items():
-                data.to_csv(f"{company_dir}/{data_type}.csv", index=False)
+                data.to_csv(pjoin(company_dir, f"{data_type}.csv"), index=False)
 
         except Exception as e:
             print(f"Failed to fetch {symbol}: {e}")
+            break
 
 # convert the tables to numpy arrays
 all_data = {data_type: {} for data_type in DATA_TYPES}
-for company in os.listdir("data"):
-    if not os.path.isdir(f"data/{company}"):
+for company in os.listdir(DATA_TMP_DIR):
+    if not os.path.isdir(pjoin(DATA_TMP_DIR, company)):
         continue
     for data_type in DATA_TYPES:
-        data = pd.read_csv(f"data/{company}/{data_type}.csv")
+        data = pd.read_csv(pjoin(DATA_TMP_DIR, company, f"{data_type}.csv"))
         data = data.to_numpy()
         data = data.astype(float)
         # replace NaN with -1
@@ -128,8 +134,8 @@ stds = {data_type: np.maximum(data.std(axis=0), MIN_STD) for data_type, data in 
 for data_type in DATA_TYPES:
     means[data_type][0] = means["stock_prices"][0]
     stds[data_type][0] = stds["stock_prices"][0]
-np.save(f"data/means.npy", means)
-np.save(f"data/stds.npy", stds)
+np.save(pjoin(DATA_TMP_DIR, "means.npy"), means)
+np.save(pjoin(DATA_TMP_DIR, "stds.npy"), stds)
 
 # normalize data
 normalized_data = {data_type: {company: (company_data_type - means[data_type]) / stds[data_type] for company, company_data_type in data_of_type.items()} for data_type, data_of_type in all_data.items()}
@@ -137,5 +143,33 @@ normalized_data = {data_type: {company: (company_data_type - means[data_type]) /
 # save normalized data
 for data_type in DATA_TYPES:
     for company in normalized_data[data_type]:
-        np.save(f"data/{company}/{data_type}.npy", normalized_data[data_type][company])
+        np.save(pjoin(DATA_TMP_DIR, company, f"{data_type}.npy"), normalized_data[data_type][company])
 print("Data collection complete.")
+
+# split to train and test and copy to data directory
+DATA_DIR = "data"
+TEST_RATIO = 0.1
+# delete the old data directory
+if os.path.exists(DATA_DIR):
+    shutil.rmtree(DATA_DIR)
+# create new data directory
+os.makedirs(DATA_DIR)
+train_dir = pjoin(DATA_DIR, "train")
+if not os.path.exists(train_dir):
+    os.makedirs(train_dir)
+test_dir = pjoin(DATA_DIR, "test")
+if not os.path.exists(test_dir):
+    os.makedirs(test_dir)
+
+companies = [company for company in os.listdir(DATA_TMP_DIR) if os.path.isdir(pjoin(DATA_TMP_DIR, company))]
+random.shuffle(companies)
+test_companies = companies[: round(len(companies) * TEST_RATIO)]
+train_companies = companies[round(len(companies) * TEST_RATIO) :]
+for company in train_companies:
+    shutil.copytree(pjoin(DATA_TMP_DIR, company), pjoin(train_dir, company))
+for company in test_companies:
+    shutil.copytree(pjoin(DATA_TMP_DIR, company), pjoin(test_dir, company))
+
+# copy mean and std
+shutil.copy(pjoin(DATA_TMP_DIR, "means.npy"), pjoin(DATA_DIR, "means.npy"))
+shutil.copy(pjoin(DATA_TMP_DIR, "stds.npy"), pjoin(DATA_DIR, "stds.npy"))
